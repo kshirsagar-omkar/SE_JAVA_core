f2043e65510e94fac645ec399fa89f23362370a7
Omkar21-1 <omkarkshirsagar2005@gmail.com>
2024-08-01 22:42:20 +0530

    HelloWorld Program Explained

---
+++ Hello_world/Hello.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Hello
+{
+	public static void main(String []args)
+	{
+		System.out.println("Hello, World!");
+	}
+}
+
+
+//Any Thing Starts With Capital Letter Programmer can Directly Undrstand it is "Class" in Java
+
+/*
+	public -> available for all
+
+	static -> bina object ch methods kiva data members acces karu shakto		
+
+	println -> is a public Method of PrintStream class
+
+	out -> is a object of PrintStream class
+			Which is Declared as public & static data member in System class
+
+	PrintStream -> is a class Defined in java.io* Package
+
+	System -> is a class Defined in java.lang Package
+
+*/
