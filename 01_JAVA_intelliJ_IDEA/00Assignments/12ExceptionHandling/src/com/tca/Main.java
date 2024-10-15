8c10b5f21c167462de1324db436087c987539cef
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2024-10-15 23:49:58 +0530

    12 - Assignment invalidlogin

--- 01_JAVA_intelliJ_IDEA/00Assignments/12ExceptionHandling/src/com/tca/Main.java
+++ 01_JAVA_intelliJ_IDEA/00Assignments/12ExceptionHandling/src/com/tca/Main.java
@@ -1,6 +1,7 @@
 package com.tca;
 
 import com.tca.entities.Login;
+import com.tca.exceptions.InvalidLogin;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,15 +9,17 @@ import java.io.InputStreamReader;
 import java.nio.Buffer;
 
 public class Main {
+
     public static void main(String[] args) {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int numberOfUsers = 1;
+        boolean userFound = false;
 
         Login []loginInfos = new Login[numberOfUsers];
 
         //Sign Up
-        System.out.println("\n-- Sign Up First Time Login --\n");
+        System.out.println("\n-- Sign Up / First Time Login --\n");
         for (int i = 0; i < loginInfos.length; i++) {
             try {
                 loginInfos[i] = new Login();
@@ -33,9 +36,42 @@ public class Main {
             }
 
         }
-        System.out.println();
+        System.out.println("Sign Up Completed Successfully !!!\n");
 
-        displayUserInfo(loginInfos);
+
+        //Assignment Code starts from here
+        System.out.println("--Login To Get Access--");
+        String userName = getStringInput(br, "Enter UserName : ");
+        String password = getStringInput(br, "Enter Password : ");
+
+        try {
+            userFound = false;
+            for (int i = 0; i < loginInfos.length; i++) {
+                if( loginInfos[i].getName().equals(userName) && loginInfos[i].getPassword().equals(password) ){
+                    userFound = true;
+                }
+            }
+
+            if(userFound == false) {
+                throw new InvalidLogin("UserName & Password Does Not Match in DataBase!!!");
+            }
+        }
+        catch (InvalidLogin il) {
+            System.out.println(il.getMessage());
+//            il.printStackTrace();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+//            e.printStackTrace();
+        }
+
+        if(userFound == true){
+            System.out.println("Granted Access Successfully :)");
+        }
+
+
+
+//        displayUserInfo(loginInfos);
 
 
 
---
+++ 01_JAVA_intelliJ_IDEA/00Assignments/12ExceptionHandlingItem02/src/com/tca/exceptions/OverPriceException.java
@@ -0,0 +1,7 @@
+package com.tca.exceptions;
+
+public class OverPriceException extends RuntimeException {
+    public OverPriceException(String message) {
+        super(message);
+    }
+}
